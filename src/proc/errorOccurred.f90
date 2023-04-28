aa1920242c6f473e2e0c2e2a5b01885455e50a5b
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-04-28 14:27:31 +0900

    added procedure to check if a status has a message

--- src/proc/errorOccurred.f90
+++ src/proc/errorOccurred.f90
@@ -10,9 +10,9 @@ module errstat_proc_errorOccurred
     implicit none
     private
     public :: error_occurred
+    public :: has_message
 
 contains
-
     !>Returns `.true.` if an `error_stat_type` object caught an error.
     !>Returns `.false.` if No error occurred or err is not present.
     logical function error_occurred(err)
@@ -27,4 +27,21 @@ contains
             error_occurred = .false.
         end if
     end function error_occurred
+
+    !>Returns `.true.` if No error occurred and message length is longer than 0
+    !>and returns `.false.` elsewhere.
+    logical function has_message(err)
+        use :: errstat_constant_status
+        implicit none
+        type(error_stat_type), intent(in), optional :: err
+            !! an error status object.
+
+        has_message = .false.
+        if (present(err)) then
+            if (err%get_status() == success_status_code .and. &
+                len(err%get_message()) > 0) then
+                has_message = .true.
+            end if
+        end if
+    end function has_message
 end module errstat_proc_errorOccurred
